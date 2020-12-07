6b09e733b2ff6fb3bfcd899b4e1d178b321b932e
etherscam <40215726+etherscam@users.noreply.github.com>
2020-12-07 18:34:57 +0300

    Update ALERT.py

--- ALERT.py
+++ ALERT.py
@@ -25,7 +25,7 @@ json={
 }
 eponch = []
 prev = 24205843
-my_call = 0
+call_me = 0
 while True:
     try:
         r = requests.post('https://rpc.mainnet.near.org', json=json)
@@ -37,7 +37,7 @@ while True:
                 num_produced = i['num_produced_blocks']
                 num_expected = i['num_expected_blocks']
                 diff =num_expected - num_produced
-                if diff>=int(config['T_BOT']['THRESHOLD']) and my_call<=2:
+                if diff>=int(config['T_BOT']['THRESHOLD']) and call_me<=2:
                     print('ALARM')
                     send_message('*[ALARM]*\n\ncheck your node\r\nProdused block:%s\r\nExpected block:%s' %(num_produced, num_expected), chat_id)
                     requests.post(config['T_BOT']['CALL_API'])
@@ -51,7 +51,7 @@ while True:
         if current != prev:
             prev=current
             send_message('New Eponch No: %s' %current, chat_id)
-            my_call = 0
+            call_me = 0
             sleep(60)
             continue
         sleep(180)
